ff436d84ec3300168c018048328c2efdd6682c63
ANANDHU KRISHNA CS <150225901+Aniyobugs@users.noreply.github.com>
2026-01-03 16:05:32 +0530

    Add files via upload

--- project/todo/models.py
+++ project/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TODO_DB(models.Model):
+    title=models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+    
+    def __str__(self):
+        return self.title
+    
+    
